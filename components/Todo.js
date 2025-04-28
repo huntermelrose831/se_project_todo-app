3cb4e0ead6baa392671c8a8c775813392838fca8
huntermelrose <huntermelrose831@gmail.com>
2025-04-27 17:16:09 -0700

    finished revisions

--- components/Todo.js
+++ components/Todo.js
@@ -9,11 +9,17 @@ class Todo {
       this._data.completed = !this._data.completed;
       this._todoCheckboxEl.checked = this._data.completed;
     });
+
+    this._deleteButton.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
   }
 
   _generateCheckboxEl() {
     this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
     this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._deleteButton = this._todoElement.querySelector(".todo__delete-btn");
+
     this._todoCheckboxEl.checked = this._data.completed;
     this._todoCheckboxEl.id = `todo-${this._data.id}`;
     this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
